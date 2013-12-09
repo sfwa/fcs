82ad97b5fc1cee0135155e07a1c5094ddba8d989
Ben Dyer <ben.dyer@taguchimail.com>
2013-12-10 02:38:20 +1100

    Updated CDCE62002 driver to reflect register layout in new datasheet

--- cpld/cdce62002_program.v
+++ cpld/cdce62002_program.v
@@ -26,21 +26,38 @@ These parameters are stable enough for the DSP to boot and complete testing,
 but the PLL doesn't lock if 1v8 is high:
 b7870061
 54200080
+
+These parameters are also suggested by the CDCE62002 tool given our component
+values and tolerances:
+b782a001
+56320080
 */
 
+/*
+Reads bottom-to-top. Write config registers, wait a while, trigger a
+calibration, then wait a bit longer
+*/
 wire[511:0] data_out = {
-    32'h61003bf2, 4'd0,
-    32'h60003bf2, 4'd0,
-    32'h61003bf2, 4'd0,
+    32'h00000000, 4'd0,
+    32'h610233f2, 4'd0,
+    32'h600233f2, 4'd0,
+    32'h610233f2, 4'd0,
+    32'h00000000, 4'd0,
+    32'h00000000, 4'd0,
+    32'h00000000, 4'd0,
     32'h00000000, 4'd0,
     32'hb7870061, 4'd0,
     32'h54200080, 8'd0
 };
 
 wire[511:0] le_out  = {
+    32'h00000000, 4'd0,
     32'hffffffff, 4'd0,
     32'hffffffff, 4'd0,
     32'hffffffff, 4'd0,
+    32'h00000000, 4'd0,
+    32'h00000000, 4'd0,
+    32'h00000000, 4'd0,
     32'h00000000, 4'd0,
     32'hffffffff, 4'd0,
     32'hffffffff, 8'd0
